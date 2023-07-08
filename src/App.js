7c85bcee14198af0ab413ea174b8987da1703761
Abhishek Singh <singhabhishek6996@gmail.com>
2023-07-09 01:37:18 +0530

    bugs fixes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Header from './components/header/Header';
 import Home from './pages/home/home';
 import MovieList from './components/movieList/movieList';
 import Movie from './pages/movieDetail/movie';
-import MovieSearch from './components/movieSearch/movieSearch';
 
 function App() {
 
@@ -25,7 +24,6 @@ function App() {
             <Routes>
                 <Route index element={<Home />}></Route>
                 <Route path="movie/:id" element={<Movie />}></Route>
-                <Route path="movie/search:id" element={<MovieSearch />}></Route>
                 <Route path="movies/:type" element={<MovieList />}></Route>
                 <Route path="/*" element={<h1>Error Page</h1>}></Route>
             </Routes>
